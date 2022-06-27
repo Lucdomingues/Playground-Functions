3a122996f3af3edc546d803ac718f08ce96b2b5e
Lucdomingues <lojalucc@outlook.com>
2022-06-27 17:41:21 -0300

    requisito01 resolucao de problemas

--- src/challenges.js
+++ src/challenges.js
@@ -2,37 +2,17 @@
 const girafa = true;
 const elefante = true;
 const macaco = false;
-
 function compareTrue(girafa, elefante, macaco) {
-  if (girafa == true && elefante == true) {
+  if (girafa === true && elefante === true) {
     return true;
-  } else if (girafa == false && elefante == true) {
+  } else if (girafa === false && elefante === true) {
     return false;
-  } else if (girafa == false && elefante == false) {
+  } else if (girafa === false && elefante === false) {
     return false;
-  } else if (girafa == true && elefante == false) {
-    return false;
-  } else if (elefante == true && macaco == true) {
-    return true;
-  } else if (elefante == false && macaco == true) {
-    return false;
-  } else if (elefante == false && macaco == false) {
-    return false;
-  } else if (elefante == true && macaco == false) {
-    return false;
-  } else if (girafa == true && macaco == true) {
-    return true;
-  } else if (girafa == false && macaco == true) {
-    return false;
-  } else if (girafa == false && macaco == false) {
-    return false;
-  } else if (girafa == true && macaco == false) {
+  } else if (girafa === true && elefante === false) {
     return false;
   }
 }
-compareTrue(girafa, elefante, macaco);
-console.log(compareTrue(girafa, macaco));
-
 // Desafio 2
 function calcArea() {
   // seu código aqui
