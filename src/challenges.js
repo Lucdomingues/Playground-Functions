666d8f87e865156ffdf4002b48b9d3f6034f509b
Lucdomingues <lojalucc@outlook.com>
2022-06-27 17:25:58 -0300

    requisito01 completed

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,37 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+const girafa = true;
+const elefante = true;
+const macaco = false;
+
+function compareTrue(girafa, elefante, macaco) {
+  if (girafa == true && elefante == true) {
+    return true;
+  } else if (girafa == false && elefante == true) {
+    return false;
+  } else if (girafa == false && elefante == false) {
+    return false;
+  } else if (girafa == true && elefante == false) {
+    return false;
+  } else if (elefante == true && macaco == true) {
+    return true;
+  } else if (elefante == false && macaco == true) {
+    return false;
+  } else if (elefante == false && macaco == false) {
+    return false;
+  } else if (elefante == true && macaco == false) {
+    return false;
+  } else if (girafa == true && macaco == true) {
+    return true;
+  } else if (girafa == false && macaco == true) {
+    return false;
+  } else if (girafa == false && macaco == false) {
+    return false;
+  } else if (girafa == true && macaco == false) {
+    return false;
+  }
 }
+compareTrue(girafa, elefante, macaco);
+console.log(compareTrue(girafa, macaco));
 
 // Desafio 2
 function calcArea() {
